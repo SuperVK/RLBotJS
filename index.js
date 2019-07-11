2462b3ecc8812294e18402f5bbcd75dc56d57492
SuperVK <victor.klomp@gmail.com>
2019-07-11 23:35:12 +0200

    added a better way of doing controllers

--- index.js
+++ index.js
@@ -2,15 +2,7 @@ const Manager = require('./src/BotManager')
 
 module.exports = {
     BaseAgent: require('./src/baseAgent'),
-    SimpleController: {
-        throttle: 0,
-        steer: 0,
-        pitch: 0,
-        roll: 0,
-        yaw: 0,
-        boost: false,
-        jump: false,
-        handbrake: false
-    },
-    Manager: Manager
+    SimpleController: require('./src/structs/SimpleController'),
+    Manager: Manager,
+    quickChats: require('./src/structs/quickchats')
 }
---
+++ src/structs/SimpleController.js
@@ -0,0 +1,12 @@
+function SimpleController() {
+    this.throttle = 0
+    this.steer = 0
+    this.pitch = 0
+    this.roll = 0
+    this.yaw = 0
+    this.boost = false
+    this.jump = false
+    this.handbrake = false
+}
+
+module.exports = SimpleController
