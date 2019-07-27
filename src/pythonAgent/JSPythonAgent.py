a06c86a0c548fb4aa46c25b22965e8134cdd9e70
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2019-07-27 07:25:51 -0700

    Comment old auto run and use better method (not tested)

--- src/pythonAgent/JSPythonAgent.py
+++ src/pythonAgent/JSPythonAgent.py
@@ -17,12 +17,14 @@ class BaseJavaScriptAgent(BaseIndependentAgent):
         self.port = self.read_port_from_file()
         self.is_retired = False
 
-        self.runner = None
-        try:
-            self.runner = subprocess.Popen([os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__), 'auto-run.bat'))])
-        except Exception as e:
-            self.runner = None
-            self.logger.error(f"A JavaScript bot with the name of {self.name} will need to be started manually. Error when running 'auto-run.bat': {str(e)}.")
+        self.auto_run_path = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__), 'auto-run.bat'))
+
+        #self.runner = None
+        #try:
+        #    self.runner = subprocess.Popen([os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__), 'auto-run.bat'))])
+        #except Exception as e:
+        #    self.runner = None
+        #    self.logger.error(f"A JavaScript bot with the name of {self.name} will need to be started manually. Error when running 'auto-run.bat': {str(e)}.")
 
     def run_independently(self, terminate_request_event):
 
@@ -41,6 +43,9 @@ class BaseJavaScriptAgent(BaseIndependentAgent):
         else:
             self.retire()
 
+    def get_helper_process_request(self):
+        return HelperProcessRequest(python_file_path=None, key=self.name + str(self.port), executable=self.auto_run_path)
+
     def retire(self):
         port = self.read_port_from_file()
         message = f"remove\n{self.index}"
@@ -53,15 +58,15 @@ class BaseJavaScriptAgent(BaseIndependentAgent):
         except ConnectionRefusedError:
             self.logger.warn("Could not connect to server!")
 
-        if self.runner is not None:
-            self.logger.info(f"Killing auto run process for bot {self.name}...")
-            try:
-                self.runner.kill()
-                self.logger.info("Success!")
-            except Exception as e:
-                self.logger.error(f"A JavaScript bot with the name of {self.name} will need to be ended manually. **YOU MAY NEED TO RESTART RLBOT.** Error when running trying to kill the bot manager: {str(e)}.")
-        else:
-            self.logger.error(f"A JavaScript bot with the name of {self.name} will need to be ended manually because it was not auto ran.")
+        #if self.runner is not None:
+        #    self.logger.info(f"Killing auto run process for bot {self.name}...")
+        #    try:
+        #        self.runner.kill()
+        #        self.logger.info("Success!")
+        #    except Exception as e:
+        #        self.logger.error(f"A JavaScript bot with the name of {self.name} will need to be ended manually. **YOU MAY NEED TO RESTART RLBOT.** Error when running trying to kill the bot manager: {str(e)}.")
+        #else:
+        #    self.logger.error(f"A JavaScript bot with the name of {self.name} will need to be ended manually because it was not auto ran.")
 
         self.is_retired = True
 
