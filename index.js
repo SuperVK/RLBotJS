4141e7b21c8207af290ea00cf5e65e625e0e5267
SuperVK <victor.klomp@gmail.com>
2019-08-09 20:52:15 +0200

    updated imports

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 module.exports = {
     BaseAgent: require('./src/baseAgent'),
-    SimpleController: require('./src/utils/SimpleController'),
-    Manager: require('./src/BotManager'),
+    SimpleController: require('./src/utils/simpleController'),
+    Manager: require('./src/botManager'),
     quickChats: require('./src/utils/quickchats'),
-    GameStateUtil: require('./src/utils/GameState'),
+    GameStateUtil: require('./src/utils/gameState'),
     ___flatStructs___: require('./src/utils/flatstructs')
 }
